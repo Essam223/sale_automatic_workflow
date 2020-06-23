14689145d19100ded16be1314e4b75e4cb1f3469
Essam Mohamed <52637449+Essam223@users.noreply.github.com>
2020-06-23 15:53:53 +0200

    workflow and confirm

---
+++ sale_automatic_confirm/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Sale Automatic Workflow',
+    'version': '12.0.1.0.0',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'author': "Akretion, "
+              "Camptocamp, "
+              "Sodexis, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/sale-workflow',
+    'depends': [
+        'sale_management',
+        'sale_stock',
+        # 'sales_team',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_view.xml',
+        # 'views/sale_workflow_process_view.xml',
+        # 'data/automatic_workflow_data.xml',
+    ],
+}
